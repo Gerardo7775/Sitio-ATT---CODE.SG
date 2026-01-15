29ad176c29d291ef2c3c95a4aa787d00b922c2da
Gerardo Adan Morelos Medina <88841180+Gerardo7775@users.noreply.github.com>
2026-01-14 23:29:04 -0600

    chore: Change Astro output mode from 'server' to 'static'.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,9 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
     // 👇 AGREGA ESTA LÍNEA CON TU URL DE VERCEL EXACTA (sin barra al final)
+    // 👇 AGREGA ESTA LÍNEA CON TU URL DE VERCEL EXACTA (sin barra al final)
     site: 'https://sitio-att-code-sg.vercel.app',
-    output: 'server',
+    output: 'static',
     adapter: vercel(),
     integrations: [
         tailwind(),
