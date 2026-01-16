82be611af061f81eed45265159966e0818764c9c
Gerardo Adan Morelos Medina <88841180+Gerardo7775@users.noreply.github.com>
2026-01-15 23:17:28 -0600

    build: Switch Astro output from server to static and remove Vercel adapter configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,17 @@ import mdx from "@astrojs/mdx";
 import markdoc from "@astrojs/markdoc";
 import react from "@astrojs/react";
 import keystatic from '@keystatic/astro';
-import vercel from '@astrojs/vercel';
+// import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
     prefetch: true,
     site: 'https://sitio-att-code-sg.vercel.app',
-    output: 'server',
-    adapter: vercel({
-        webAnalytics: { enabled: true },
-        imageService: true,
-    }),
+    output: 'static',
+    // adapter: vercel({
+    //     webAnalytics: { enabled: true },
+    //     imageService: true,
+    // }),
     integrations: [
         tailwind(),
         react(),
