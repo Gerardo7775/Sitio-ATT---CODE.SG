a1d35a43a4125c671eefd1fc3fe7519cf835b62b
Gerardo Adan Morelos Medina <88841180+Gerardo7775@users.noreply.github.com>
2026-01-15 21:01:46 -0600

    build: Disable Vercel adapter configuration in Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,18 @@ import mdx from "@astrojs/mdx";
 import markdoc from "@astrojs/markdoc";
 import react from "@astrojs/react";
 import keystatic from '@keystatic/astro';
-import vercel from '@astrojs/vercel';
+//import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
     prefetch: true,
     site: 'https://sitio-att-code-sg.vercel.app',
     output: 'static',
-    adapter: vercel({
+    /*adapter: vercel({
         webAnalytics: {
             enabled: true,
         },
-    }),
+    }),*/
     integrations: [
         tailwind(),
         react(),
